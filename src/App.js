1b59ea843b1409e6b627bedbd2afe5c651f61b70
mark-scholes <innominatus15@outlook.com>
2020-08-25 22:51:25 +0100

    created the requests I will be using and used axios to deal with the url string

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
+  fetch(
+    "https://api.themoviedb.org/3/tv/2190?api_key=cae9e457e3ca82324f9c392ea76944d5"
+  )
+    .then((respone) => respone.json())
+    .then((data) => {});
   return <div className="App">Test</div>;
 };
 
 export default App;
+
+//cae9e457e3ca82324f9c392ea76944d5
+//https://api.themoviedb.org/3/movie/76341?api_key=cae9e457e3ca82324f9c392ea76944d5
