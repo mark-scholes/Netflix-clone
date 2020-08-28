8a53d5246c459defbee5b57ae8679331547fe9f7
mark-scholes <innominatus15@outlook.com>
2020-08-28 22:01:26 +0100

    Added react-youtube and movieTrailer so the user can click on a image and a youtube search returns a video of the chosen movie. Also made it so movies that don't have a backdrop image in the api data the standard poster is used. This does mean that they will be different dimentions to the others but at least an image is shown. A better solution needs to be found going forward.

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import "../row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const opts = {
+    height: "500",
+    width: "100%",
+    playerVars: {
+      //https://developers.google.com/youtube/player-parameters,
+      autoplay: 1,
+    },
+  };
   const base_url = "https://image.tmdb.org/t/p/original/";
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name, "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+
+        .catch((error) => console.log(error));
+    }
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -15,6 +40,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     }
     fetchData();
   }, [fetchUrl]);
+  console.log(movies);
   return (
     <div className="row">
       <h2 className="row__title">{title}</h2>
@@ -22,14 +48,19 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__img ${isLargeRow ? "row__imgLarge" : ""}`}
+            // {some movies don't have the a backdrop image, in these cases use poster instead}
             src={`${base_url}/${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
+              isLargeRow || !movie.backdrop_path
+                ? movie.poster_path
+                : movie.backdrop_path
             }`}
-            alt={movie.name}
+            alt={movie?.title || movie?.name || movie?.original_name}
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
